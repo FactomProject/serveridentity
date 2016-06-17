7738e508e2d2de68eacacd9b1e3711cd91c570c3
Steven Masley <stevenmasley@gmail.com>
2016-06-17 16:49:56 -0500

    more script work

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -1,7 +1,10 @@
 #/bin/bash
-
-COUNTER=$1
- while [  $COUNTER -lt $2 ]; do
-     sh ./i/$COUNTER.sh
-     let COUNTER=COUNTER+1 
- done
+if [ $1 = "all" ]; then
+	sh ./i/*.sh
+else
+	COUNTER=$1
+	 while [  $COUNTER -lt $2 ]; do
+	     sh ./i/$COUNTER.sh
+	     let COUNTER=COUNTER+1 
+	 done
+fi
