4f68a8e3db8cccc3b9b5ecc2206969b270cd4de2
Steven Masley <stevenmasley@gmail.com>
2016-06-20 10:02:56 -0500

    scripts

--- Scripts/clean.sh
+++ Scripts/clean.sh
@@ -2,7 +2,11 @@
 
 COUNTER=$1
 rm nohup.out
- while [  $COUNTER -lt $2 ]; do
+if [ $1 = "all" ]; then
+	for each in ./i/*.sh; do rm $each; done
+else
+ while [ $COUNTER -lt $2 ]; do
      rm "./i/$COUNTER.sh"
      let COUNTER=COUNTER+1 
  done
+fi
