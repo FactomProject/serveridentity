7b9b8828907245bc625018749f62b53eaf8004c5
Steven Masley <stevenmasley@gmail.com>
2016-08-04 09:25:47 -0500

    Corrected Root Chain ID. Mistake when changing it

--- identity/varsConfig.go
+++ identity/varsConfig.go
@@ -9,7 +9,8 @@ var (
 	ProofOfWorkChainID     = []byte{0x88, 0x88, 0x88}
 	ProofOfWorkLength  int = 3 // DEFAULT = 3, set lower for faster tests
 	// For Registering Root Chain
-	RootRegisterChain string = "376395f0b4e88cf369a36818c2bd4ef2a8a17c84c58e8148f8ca9df5ed8752c1"
+	//376395f0b4e88cf369a36818c2bd4ef2a8a17c84c58e8148f8ca9df5ed8752c1
+	RootRegisterChain string = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 	// If true will show "." on screen to show user program is computing
 	ShowBruteForce bool = true
 	// Will change frequency of dots for brute forcing
